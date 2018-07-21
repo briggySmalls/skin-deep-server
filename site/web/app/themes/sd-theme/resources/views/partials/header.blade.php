7fb3e778c4151c619377c2ed3d43772ccac020c9
briggySmalls <wasso14@hotmail.com>
2018-07-21 08:56:51 +0100

    Fix bootstrap menu collapsing

--- site/web/app/themes/sd-theme/resources/views/partials/header.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/header.blade.php
@@ -1,11 +1,13 @@
 <header class="banner sticky-top">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav']) !!}
+    <div id="navbarNavDropdown" class="collapse navbar-collapse">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav']) !!}
+    </div>
     @endif
     <div class="snipcart-summary">
       <a href="#" class="snipcart-user-profile">
