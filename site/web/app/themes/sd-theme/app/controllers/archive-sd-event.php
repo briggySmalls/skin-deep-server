ea7995e59142ce3b19d93032881a26cf8d0df71f
briggySmalls <wasso14@hotmail.com>
2018-08-18 11:56:08 +0100

    WIP: Start working on an archive for events

---
+++ site/web/app/themes/sd-theme/app/controllers/archive-sd-event.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+use SdEvents\SdEventsApi;
+
+class ArchiveSdEvent extends Controller
+{
+    public function sdEventsApi() {
+        return new SdEventsApi();
+    }
+}
