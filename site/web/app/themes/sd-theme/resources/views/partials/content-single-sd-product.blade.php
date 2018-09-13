f12606ecaeb654f70f325d7f2b6738a2d5b93c7e
briggySmalls <wasso14@hotmail.com>
2018-09-13 21:16:31 +0100

    Rearrange product page photo/text

--- site/web/app/themes/sd-theme/resources/views/partials/content-single-sd-product.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/content-single-sd-product.blade.php
@@ -1,24 +1,26 @@
-<article @php post_class() @endphp>
-  <header>
-    <figure>
-      {{-- Display the featured image --}}
-      @include('partials/image-header')
-    </figure>
-    <h1 class="entry-title">{{ get_the_title() }}</h1>
-  </header>
-  <div class="entry-content">
-    {{ get_the_content() }}
-  </div>
-  <div>
-    <button
-      class="snipcart-add-item"
-      data-item-id="{{ SingleSdProduct::id() }}"
-      data-item-name="{{ SingleSdProduct::name() }}"
-      data-item-price="{{ SingleSdProduct::price() }}"
-      data-item-url="{{ SingleSdProduct::url() }}"
-      data-item-description="{{ SingleSdProduct::description()}}">
-          Buy
-    </button>
+<article @php post_class('container') @endphp>
+  <div class="row">
+    <div class="col-md">
+      <figure>
+        {{-- Display the featured image --}}
+        @include('partials/image-header')
+      </figure>
+    </div>
+    <div class="col-md">
+      <h1 class="entry-title">{{ get_the_title() }}</h1>
+      <div class="entry-content">
+        {{ get_the_content() }}
+      </div>
+      <button
+        class="snipcart-add-item"
+        data-item-id="{{ SingleSdProduct::id() }}"
+        data-item-name="{{ SingleSdProduct::name() }}"
+        data-item-price="{{ SingleSdProduct::price() }}"
+        data-item-url="{{ SingleSdProduct::url() }}"
+        data-item-description="{{ SingleSdProduct::description()}}">
+            Buy
+      </button>
+    </div>
   </div>
   @php comments_template('/partials/comments.blade.php') @endphp
 </article>
