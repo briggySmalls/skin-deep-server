2e25e03a4511d791ace7a246c4702d614540dacd
briggySmalls <wasso14@hotmail.com>
2018-09-15 11:34:36 +0100

    Cosmetic: Add comments to template

--- site/web/app/themes/sd-theme/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/page-header.blade.php
@@ -1,10 +1,12 @@
 <div class="page-header">
+  {{-- Display a category image if this is a category archive --}}
   @if (is_category())
     <figure>
       {!! Category::image( 'large' ) !!}
     </figure>
   @endif
   <h1>{!! App::title() !!}</h1>
+  {{-- Display a category description if this is a category archive --}}
   @if (is_category())
     {!! Category::description() !!}
   @endif
