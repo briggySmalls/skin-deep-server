71853fef6b2955bf3859c0f5ea5bd2dc95341976
briggySmalls <wasso14@hotmail.com>
2018-09-15 11:38:48 +0100

    Remove unnecessary row breaks
    To ensure equal width columns, you either need:
    a) Fluid-width columns of type "col-{size}" and need row breaks
    b) Enforce column width using a class of type "col-{size}-{width}"
    
    This removes a) as we are already using b)

--- site/web/app/themes/sd-theme/resources/views/archive.blade.php
+++ site/web/app/themes/sd-theme/resources/views/archive.blade.php
@@ -13,7 +13,6 @@
   {{-- Display posts in a grid --}}
   <div class="container-fluid">
     <div class="row">
-      @php $i = 0; @endphp
       @while (have_posts()) @php the_post() @endphp
         <div class="col-md-{{ 12 / $column_count }}">
           {{-- Wrap the entire card in a link --}}
@@ -21,11 +20,6 @@
             @include('partials.content-archive')
           </a>
         </div>
-        {{-- Break row on final item --}}
-        @if (($i + 1) % $column_count == 0)
-          <div class="w-100"></div>
-        @endif
-        @php $i++; @endphp
       @endwhile
     </div>
   </div>
