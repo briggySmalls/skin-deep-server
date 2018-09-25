a0c5313d2b5a89b13d0c85d13b6e5d4b066ecfbc
briggySmalls <wasso14@hotmail.com>
2018-09-25 22:32:29 +0100

    Only create categories paragraph if there are categories to display

--- site/web/app/mu-plugins/sd-articles/resources/views/partials/single_post.blade.php
+++ site/web/app/mu-plugins/sd-articles/resources/views/partials/single_post.blade.php
@@ -3,15 +3,19 @@
   'articles::partials.featured-media',
   ['image_size' => 'post-thumbnail'])
 <div class="card-body">
-  {{-- Display category --}}
-  <p class="category-link">
-    @foreach ($post->categories() as $category)
-      <a href="{{ get_category_link($category) }}">
-        {{ $category->name }}
-      </a>
-    @endforeach
-  </p>
+  @php
+  $categories = $post->categories();
+  @endphp
+  @if ($categories)
+    {{-- Display category --}}
+    <p class="category-link">
+      @foreach ($categories as $category)
+        <a href="{{ get_category_link($category) }}">
+          {{ $category->name }}
+        </a>
+      @endforeach
+    </p>
+  @endif
   {{-- Display title --}}
   <h3 class="card-title">{{ $post->post_title }}</h3>
-  {{-- TODO: Display excerpt? --}}
 </div>
