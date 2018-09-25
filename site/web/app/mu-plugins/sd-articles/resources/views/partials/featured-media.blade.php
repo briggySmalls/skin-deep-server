62e9fd8ecc89e199e217cac88edf3bf758421c07
briggySmalls <wasso14@hotmail.com>
2018-09-25 19:50:54 +0100

    Fix #40 by only showing images in grid/slider (not videos)
    Also fixes #2 #3

--- site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
+++ site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
@@ -1,9 +1,7 @@
-@if ($post->video())
-  {{-- Feature video takes precedence --}}
-  <div class="embed-responsive embed-responsive-16by9">
-    {!! $post->video() !!}
-  </div>
-@elseif ($post->hasImage())
-  {{-- Otherwise display the featured image --}}
-  {!! get_the_post_thumbnail($post->ID, $image_size, isset($image_classes) ? ['class' => $image_classes] : []) !!}
+@if ($post->hasImage())
+  {{-- Display the featured image --}}
+  {!! get_the_post_thumbnail(
+    $post->ID,
+    $image_size,
+    isset($image_classes) ? ['class' => $image_classes] : []) !!}
 @endif
