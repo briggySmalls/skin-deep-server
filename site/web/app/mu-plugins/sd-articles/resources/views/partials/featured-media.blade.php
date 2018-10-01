af2fd6a540f650a62693877d8006390291137e25
briggySmalls <wasso14@hotmail.com>
2018-10-02 00:47:50 +0100

    Simplify how sizes and classes are specified

--- site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
+++ site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
@@ -4,9 +4,6 @@
       <i class="fas fa-video video-icon" title="Video article"></i>
     @endif
     {{-- Display the featured image --}}
-    {!! get_the_post_thumbnail(
-      $post->ID,
-      isset($image_size) ? $image_size : 'post-thumbnail',
-      isset($image_classes) ? ['class' => $image_classes] : []) !!}
+    {!! $post->image($image_classes, $image_sizes) !!}
   </div>
 @endif
