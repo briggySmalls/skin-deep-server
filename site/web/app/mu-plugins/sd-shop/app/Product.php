f6a97f32f1bd2da77ecf3819f2d95d25a26b73f1
briggySmalls <wasso14@hotmail.com>
2018-10-02 03:14:03 +0100

    Be cheeky and get jetpack to make magazine images portrait

--- site/web/app/mu-plugins/sd-shop/app/Product.php
+++ site/web/app/mu-plugins/sd-shop/app/Product.php
@@ -23,4 +23,25 @@ class Product extends Post
     {
         return get_field('sd_product_in_stock', $this->post->ID);
     }
+
+    public function image($classes=false, $sizes=false, $size="post-thumbnail")
+    {
+        if (has_term('magazines', 'sd-product-cat', $this->post->ID) &&
+                class_exists('Jetpack') &&
+                \Jetpack::is_module_active('photon')) {
+            // Product is a magazine, and photon is active - let's make it portrait!
+            $img_id = get_post_thumbnail_id($this->post->ID);
+            // Get the srcset and modify it to be portrait
+            $img_srcset = wp_get_attachment_image_srcset($img_id, 'post-thumbnail');
+            $transformed_srcset = preg_replace(
+                "/((?<path>.+?)(?<width>\d+)x(?<height>\d+)(?<ext>\..+?) (?<width_2>\d+w))/",
+                "$2$4x$3$5 $4w",
+                $img_srcset);
+            return get_the_post_thumbnail(
+                $this->post->ID,
+                'post-thumbnail',
+                ['srcset' => $transformed_srcset]);
+        }
+        return parent::image($classes, $sizes, $size);
+    }
 }
