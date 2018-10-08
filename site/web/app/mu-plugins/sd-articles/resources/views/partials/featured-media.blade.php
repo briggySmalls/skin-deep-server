feba195099bb35b925fbc072e185b7b6b4a6533c
briggySmalls <wasso14@hotmail.com>
2018-10-08 01:46:34 +0100

    Undo work on video icons

--- site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
+++ site/web/app/mu-plugins/sd-articles/resources/views/partials/featured-media.blade.php
@@ -1,8 +1,5 @@
 @if ($post->hasImage())
   <div class="featured-image postition-relative">
-    @if ($post->hasVideo())
-      <i class="fas fa-video video-icon" title="Video article"></i>
-    @endif
     {{-- Display the featured image --}}
     {!! $post->image($image_classes ?? false, $image_sizes ?? false) !!}
   </div>
--- site/web/app/themes/sd-theme/resources/assets/scripts/autoload/_fontawesome.js
+++ site/web/app/themes/sd-theme/resources/assets/scripts/autoload/_fontawesome.js
@@ -1,12 +1,12 @@
 // Import then needed Font Awesome functionality
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 // Import the required icons
-import { faClock, faCalendarAlt, faMapMarkerAlt, faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faCalendarAlt, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 // Import brand icons
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 // Add the imported icons to the library
-library.add(faClock, faCalendarAlt, faMapMarkerAlt, faFacebookF, faVideo);
+library.add(faClock, faCalendarAlt, faMapMarkerAlt, faFacebookF);
 
 // Tell FontAwesome to watch the DOM and add the SVGs when it detects icon markup
 dom.watch();
