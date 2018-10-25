150a2df13d256b841a6869b2ca6906ee3a3ab1a6
briggySmalls <wasso14@hotmail.com>
2018-10-26 00:53:46 +0100

    Attempt to fix size constraining images
    This is mainly copying stack magazines' implementation

--- site/web/app/themes/sd-theme/resources/views/partials/shop/archive-product.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/shop/archive-product.blade.php
@@ -1,13 +1,15 @@
 @php $product = new SkinDeep\Shop\Product($post); @endphp
 <figure class="product-figure" style="background-color: {{ $product->backgroundColour() }}">
   @if ($product->hasImage())
+  <div class="product-image">
     @include(
       'partials.components.image-header',
       [
         'post' => $product,
         'image_sizes' => \SkinDeep\Articles\PostsPreview::sizes($column_count)
       ])
-  @endif
+    @endif
+  </div>
 </figure>
 <div class="card-body">
   <h5 class="card-title">{!! $product->title() !!}</h5>
