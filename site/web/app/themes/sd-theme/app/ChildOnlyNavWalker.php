b4e1da7073bbe4eba62f6c421911d1e1c6a00285
briggySmalls <wasso14@hotmail.com>
2018-11-05 00:42:23 +0000

    Tidy up slightly

--- site/web/app/themes/sd-theme/app/ChildOnlyNavWalker.php
+++ site/web/app/themes/sd-theme/app/ChildOnlyNavWalker.php
@@ -6,22 +6,23 @@ class ChildOnlyNavWalker extends \Walker_Nav_Menu
 {
     function start_lvl(&$output, $depth=0, $args=array()) {
         if($depth == 0) {
+            // We start at level 1
             return;
         }
         parent::start_lvl($output, $depth,$args);
     }
 
-    // Don't end the top level
     function end_lvl(&$output, $depth=0, $args=array()) {
         if($depth == 0) {
+            // We start at level 1
             return;
         }
         parent::end_lvl($output, $depth,$args);
     }
 
-    // Don't print top-level elements
     function start_el(&$output, $item, $depth=0, $args=array(), $id=0) {
         if($depth == 0){
+            // We start at level 1
             return;
         }
         parent::start_el($output, $item, $depth, $args);
@@ -29,17 +30,16 @@ class ChildOnlyNavWalker extends \Walker_Nav_Menu
 
     function end_el(&$output, $item, $depth=0, $args=array()) {
         if($depth == 0){
+            // We start at level 1
             return;
         }
         parent::end_el($output, $item, $depth, $args);
     }
 
-    // Only follow down one branch
-    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
-
+    function display_element($element, &$children_elements, $max_depth, $depth=0, $args, &$output) {
         // Check if element as a 'current element' class
-        $current_element_markers = array( 'current-menu-item', 'current-menu-parent', 'current-menu-ancestor' );
-        $current_class = array_intersect( $current_element_markers, $element->classes );
+        $current_element_markers = ['current-menu-item', 'current-menu-parent', 'current-menu-ancestor'];
+        $current_class = array_intersect($current_element_markers, $element->classes);
 
         // If element has a 'current' class, it is an ancestor of the current element
         $ancestor_of_current = !empty($current_class);
