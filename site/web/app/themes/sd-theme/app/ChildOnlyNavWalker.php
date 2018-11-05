6903b8126ec7d963efcff10e63f59605df9342b0
briggySmalls <wasso14@hotmail.com>
2018-11-05 00:42:57 +0000

    Lint

--- site/web/app/themes/sd-theme/app/ChildOnlyNavWalker.php
+++ site/web/app/themes/sd-theme/app/ChildOnlyNavWalker.php
@@ -4,39 +4,44 @@ namespace SkinDeep\Theme;
 
 class ChildOnlyNavWalker extends \Walker_Nav_Menu
 {
-    function start_lvl(&$output, $depth=0, $args=array()) {
-        if($depth == 0) {
+    function start_lvl(&$output, $depth = 0, $args = array())
+    {
+        if ($depth == 0) {
             // We start at level 1
             return;
         }
-        parent::start_lvl($output, $depth,$args);
+        parent::start_lvl($output, $depth, $args);
     }
 
-    function end_lvl(&$output, $depth=0, $args=array()) {
-        if($depth == 0) {
+    function end_lvl(&$output, $depth = 0, $args = array())
+    {
+        if ($depth == 0) {
             // We start at level 1
             return;
         }
-        parent::end_lvl($output, $depth,$args);
+        parent::end_lvl($output, $depth, $args);
     }
 
-    function start_el(&$output, $item, $depth=0, $args=array(), $id=0) {
-        if($depth == 0){
+    function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+    {
+        if ($depth == 0) {
             // We start at level 1
             return;
         }
         parent::start_el($output, $item, $depth, $args);
     }
 
-    function end_el(&$output, $item, $depth=0, $args=array()) {
-        if($depth == 0){
+    function end_el(&$output, $item, $depth = 0, $args = array())
+    {
+        if ($depth == 0) {
             // We start at level 1
             return;
         }
         parent::end_el($output, $item, $depth, $args);
     }
 
-    function display_element($element, &$children_elements, $max_depth, $depth=0, $args, &$output) {
+    function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output)
+    {
         // Check if element as a 'current element' class
         $current_element_markers = ['current-menu-item', 'current-menu-parent', 'current-menu-ancestor'];
         $current_class = array_intersect($current_element_markers, $element->classes);
@@ -45,10 +50,10 @@ class ChildOnlyNavWalker extends \Walker_Nav_Menu
         $ancestor_of_current = !empty($current_class);
 
         // If this is a top-level link and not the current, or ancestor of the current menu item - stop here.
-        if ( 0 == $depth && !$ancestor_of_current){
+        if (0 == $depth && !$ancestor_of_current) {
             return;
         }
 
-        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+        parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
     }
 }
