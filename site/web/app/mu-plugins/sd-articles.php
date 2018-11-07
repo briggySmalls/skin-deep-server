1e0ddadbfea435693221681840346d84ab2b9ada
briggySmalls <wasso14@hotmail.com>
2018-11-07 02:05:41 +0000

    Rework events to implement filtering on status
    Involves a refactor to use the Loader class from sd-shop.
    Currently order in which plugins are loaded matters, which is bad

--- site/web/app/mu-plugins/sd-articles.php
+++ site/web/app/mu-plugins/sd-articles.php
@@ -1,4 +1,5 @@
 <?php
 // Include custom plugins
+require_once WPMU_PLUGIN_DIR.'/sd-shop/app/sd-shop.php';
 require WPMU_PLUGIN_DIR.'/sd-articles/app/sd-articles.php';
 require WPMU_PLUGIN_DIR.'/sd-events/app/sd-events.php';
