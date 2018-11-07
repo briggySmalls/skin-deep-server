f69fd81286e5f1bf63869545996c8c822671a9c1
briggySmalls <wasso14@hotmail.com>
2018-11-07 02:12:27 +0000

    Activate development mode for Jetpack on dev build

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -9,6 +9,7 @@ Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
+Config::define('JETPACK_DEV_DEBUG', true);
 
 ini_set('display_errors', 1);
 
