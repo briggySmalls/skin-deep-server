7b560c31fd1ecd74c6bca556159482a5abb8140f
briggySmalls <wasso14@hotmail.com>
2018-11-22 02:17:21 +0000

    Fix styles for blocks

--- site/web/app/mu-plugins/sd-articles/resources/views/preview-widget.blade.php
+++ site/web/app/mu-plugins/sd-articles/resources/views/preview-widget.blade.php
@@ -2,12 +2,14 @@
   Widget output template
   --}}
 {{-- Display the category title --}}
-<h2 class="title">
-  <a href={{ $url }}>
-    {{ $title }}
-  </a>
-</h2>
-{{-- Create grid of posts --}}
-@include("articles::partials.grid", ['posts' => $posts])
-{{-- Display see more link --}}
-<div class="see-more"><a href="{{ $url }}">See more articles</a></div>
+<div class="preview-posts">
+  <h2 class="title">
+    <a href={{ $url }}>
+      {{ $title }}
+    </a>
+  </h2>
+  {{-- Create grid of posts --}}
+  @include("articles::partials.grid", ['posts' => $posts])
+  {{-- Display see more link --}}
+  <div class="see-more"><a href="{{ $url }}">See more articles</a></div>
+</div>
