51688ae0276d8c3935e6c716f3f3cd34f49ac723
briggySmalls <wasso14@hotmail.com>
2018-11-28 01:20:14 +0000

    Improve template (use navbar styles)

--- site/web/app/themes/sd-theme/resources/views/layouts/app.blade.php
+++ site/web/app/themes/sd-theme/resources/views/layouts/app.blade.php
@@ -20,19 +20,35 @@
     @include('partials.footer')
     @php wp_footer() @endphp
   <script id="cart-content-text" type="text/template">
-    <div>
-      <a href="#" class="snipcart-checkout">
-        Cart
-      </a>
-      <a href="#" class="snipcart-user-profile">
-        Orders
-      </a>
-      <a href="#" class="snipcart-edit-profile">
-        Profile
-      </a>
-      <a href="#" class="snipcart-user-logout">
-        Logout
-      </a>
+    <nav class="navbar navbar-light">
+      <ul class="navbar-nav mr-3">
+        <li class="nav-item">
+          <a href="#" class="snipcart-checkout">
+            Cart
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-3">
+        <li class="nav-item">
+          <a href="#" class="snipcart-user-profile">
+            Orders
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-3">
+        <li class="nav-item">
+          <a href="#" class="snipcart-edit-profile">
+            Profile
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-3">
+        <li class="nav-item">
+          <a href="#" class="snipcart-user-logout">
+            Logout
+          </a>
+        </li>
+      </ul>
     </div>
   </script>
   </body>
