2b498873103f784755366d4279960895abf59ea6
briggySmalls <wasso14@hotmail.com>
2018-12-04 00:09:00 +0000

    WIP: Create menu using CSS grid

--- site/web/app/themes/sd-theme/resources/views/partials/header.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/header.blade.php
@@ -1,43 +1,40 @@
 <header id="main-header" class="banner">
       {{-- Don't bother with a link, it's supplied already --}}
   <nav class="navbar top-level">
-    @if (has_custom_logo())
-      @php the_custom_logo() @endphp
-    @else
-      <a class="navbar-brand" href="{{ home_url('/') }}">
-        {{ get_bloginfo('name', 'display') }}
-      </a>
-    @endif
-    <button class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div id="navbarNavDropdown" class="collapse navbar-collapse">
-      {{-- Main menu navigation --}}
-      @if (has_nav_menu('primary_navigation'))
-        {{-- Standard 'top-level' menu items --}}
-        {!! wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'depth' => 1,
-          'menu_class' => 'header-menu']) !!}
-      @endif
-      {{-- Search bar --}}
-      {!! get_search_form() !!}
+    <div class="navigation-grid">
+      <div class="logo">
+        @if (has_custom_logo())
+          @php the_custom_logo() @endphp
+        @else
+          <a class="navbar-brand" href="{{ home_url('/') }}">
+            {{ get_bloginfo('name', 'display') }}
+          </a>
+        @endif
+      </div>
+      <div class="top-nav">
+        {{-- Main menu navigation --}}
+        @if (has_nav_menu('primary_navigation'))
+          {{-- Standard 'top-level' menu items --}}
+          {!! wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'depth' => 1,
+            'menu_class' => 'header-menu']) !!}
+        @endif
+      </div>
+      <div class="sub-nav">
+        {{-- Add sub-menu with custom walker for children --}}
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'walker' => new SkinDeep\Theme\ChildOnlyNavWalker(),
+            'depth' => 0,
+            'menu_class' => 'header-menu']) !!}
+        @endif
+      </div>
+      <div class="search">
+        {{-- Search bar --}}
+        {!! get_search_form() !!}
+      </div>
     </div>
   </nav>
-  {{-- Add sub-menu with custom walker for children --}}
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="navbar sub-menu">
-      {!! wp_nav_menu([
-        'theme_location' => 'primary_navigation',
-        'walker' => new SkinDeep\Theme\ChildOnlyNavWalker(),
-        'depth' => 0,
-        'menu_class' => 'header-menu']) !!}
-    </nav>
-  @endif
 </header>
