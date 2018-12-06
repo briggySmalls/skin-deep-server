fc331a364a26b4e4979c07c3d9356995cd63e3e2
briggySmalls <wasso14@hotmail.com>
2018-12-06 03:47:02 +0000

    Only use shortcode if Jetpack enabled

--- site/web/app/themes/sd-theme/resources/views/partials/single/sd-product.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/single/sd-product.blade.php
@@ -22,8 +22,10 @@ assert(is_a($post, 'SkinDeep\Shop\Product'));
       </div>
     </div>
   </div>
-  <hr/>
   {{-- Output related posts in a special location --}}
-  {!! do_shortcode( '[jetpack-related-posts]' ) !!}
+  @if (class_exists('Jetpack_RelatedPosts'))
+  <hr/>
+    {!! do_shortcode( '[jetpack-related-posts]' ) !!}
+  @endif
   @php comments_template('/partials/comments.blade.php') @endphp
 </article>
