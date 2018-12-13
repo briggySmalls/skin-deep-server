a778f265b9c7200ed38ca8cf470a27a62e29917d
briggySmalls <wasso14@hotmail.com>
2018-12-13 00:55:26 +0000

    Fix #151: Check for gutenberg global

--- site/web/app/mu-plugins/skin-deep/resources/assets/scripts/acf.js
+++ site/web/app/mu-plugins/skin-deep/resources/assets/scripts/acf.js
@@ -2,11 +2,56 @@
 
 const FEATURED_VIDEO_FIELD = 'field_5b21a633286bf';
 
+// Listen for data changes
+jQuery(document).ready(function () {
+  if ((typeof wp === 'object') && (wp.hasOwnProperty('data'))) {
+    // We are in the Gutenberg editor
+    wp.data.subscribe(() => {
+      if (isformatChanged()) {
+        // Get the current value
+        var format = getCurrentFormat();
+        // Get the featured video ACF field
+        var featured_video_field = acf.getField(FEATURED_VIDEO_FIELD);
+        // Toggle its visibility
+        if (format === 'video') {
+          featured_video_field.show();
+        } else {
+          featured_video_field.hide();
+        }
+      }
+    });
+
+    // Validate that all videos have a video source
+    acf.add_filter('validation_complete', function (json) {
+      // Check if post has video if it's video format
+      if ((getCurrentFormat() === 'video') && !acf.getField(FEATURED_VIDEO_FIELD).val()) {
+        if (json.errors == 0) {
+          json.errors = [];
+        }
+        json.errors.push({
+          'input': `acf[${FEATURED_VIDEO_FIELD}]`,
+          'message': 'Featured video required for video posts',
+        });
+        json.valid = 0;
+        return json
+      }
+      return json;
+    });
+  }
+});
+
+/**
+ * @brief      Helper function to get the current post format.
+ * @return     The format.
+ */
 function getCurrentFormat() {
   return wp.data.select('core/editor').getEditedPostAttribute('format');
 }
 
-// Configure featured video to hide/show on format
+/**
+ * @brief      Determine if format is different from previous value
+ * @return     true if format has changed, otherwise false
+ */
 function isformatChanged() {
     let previousValue = currentValue;
     var currentValue = getCurrentFormat();
@@ -15,36 +60,3 @@ function isformatChanged() {
     }
     return false;
 }
-
-// Listen for data changes
-wp.data.subscribe(() => {
-  if (isformatChanged()) {
-    // Get the current value
-    var format = getCurrentFormat();
-    // Get the featured video ACF field
-    var featured_video_field = acf.getField(FEATURED_VIDEO_FIELD);
-    // Toggle it's visibility
-    if (format === 'video') {
-      featured_video_field.show();
-    } else {
-      featured_video_field.hide();
-    }
-  }
-});
-
-// Validate that all videos have a video source
-acf.add_filter('validation_complete', function (json) {
-  // Check if post has video if it's video format
-  if ((getCurrentFormat() === 'video') && !acf.getField(FEATURED_VIDEO_FIELD).val()) {
-    if (json.errors == 0) {
-      json.errors = [];
-    }
-    json.errors.push({
-      'input': `acf[${FEATURED_VIDEO_FIELD}]`,
-      'message': 'Featured video required for video posts',
-    });
-    json.valid = 0;
-    return json
-  }
-  return json;
-});
