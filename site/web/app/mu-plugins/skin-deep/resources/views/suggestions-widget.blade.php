30950b0e42ef719e2bcaa17f07d2d395fe07dc46
briggySmalls <wasso14@hotmail.com>
2018-12-14 00:49:47 +0000

    Fix #74: Use the same styling as on theme

--- site/web/app/mu-plugins/skin-deep/resources/views/suggestions-widget.blade.php
+++ site/web/app/mu-plugins/skin-deep/resources/views/suggestions-widget.blade.php
@@ -7,7 +7,8 @@
     <p class="lead">
       More articles written by
       @foreach ($authors as $author)
-        <a href="{{ get_term_link($author) }}">{{ $author->name }}</a>
+        <a href="{{ get_term_link($author) }}">
+          {{ $author->name }}</a>@if (!$loop->last)<span>,&#32;</span>@endif
       @endforeach
     </p>
     <p class="lead">
@@ -15,8 +16,7 @@
         More articles in categories:
         @foreach ($categories as $category)
             <a href="{{ get_category_link($category) }}">
-              {{ $category->name }}
-            </a>
+              {{ $category->name }}</a>@if (!$loop->last)<span>,&#32;</span>@endif
         @endforeach
       @endif
     </p>
