4789aa0004c091f8cf1869f58288ac8a3a257110
briggySmalls <wasso14@hotmail.com>
2018-12-16 19:35:40 +0000

    Fix #121: Rename and add description to plugin/theme

--- site/web/app/mu-plugins/skin-deep/app/skin-deep.php
+++ site/web/app/mu-plugins/skin-deep/app/skin-deep.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * @wordpress-plugin
- * Plugin Name: Skin Deep articles
- * Plugin URI: https://github.com/briggySmalls/skin-deep-server/
- * Description: Customisation of wordpress posts to display articles
- * Version: 1.0.0
- * Author: Sam Briggs
- * Author URI: https://github.com/briggySmalls/
- * License: MIT
- */
 
 namespace SkinDeep;
 
