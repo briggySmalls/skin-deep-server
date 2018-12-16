78eb29bb35fc83c33cc78ef98fb8ffbcd00eecd8
briggySmalls <wasso14@hotmail.com>
2018-12-16 22:41:02 +0000

    Fix #127: Enable google analytics

---
+++ site/web/app/mu-plugins/skin-deep/resources/assets/scripts/public.js
@@ -0,0 +1,8 @@
+// Run google analytics script
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// Pass the tracking ID (substituted at compile time)
+gtag('config', process.env.GOOGLE_TRACKING_ID);
+console.log(process.env.GOOGLE_TRACKING_ID);
