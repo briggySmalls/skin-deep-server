309d38acaaebffc9aaf8b11458e31002e924ef73
briggySmalls <wasso14@hotmail.com>
2018-12-19 01:02:30 +0000

    Update snipcart script to current API

--- site/web/app/mu-plugins/skin-deep/resources/assets/scripts/snipcart.js
+++ site/web/app/mu-plugins/skin-deep/resources/assets/scripts/snipcart.js
@@ -3,20 +3,21 @@
  */
 /*global Snipcart*/
 (function($) {
+  // Wait for snipcart script to have loaded
   $('#snipcart').load(() => {
     console.assert(typeof Snipcart !== 'undefined', "Snipcart not found");
-    Snipcart.execute('bind', 'cart.opened', function() {
-        // Immediately unbind this event
-        Snipcart.execute('unbind', 'cart.opened');
+    Snipcart.subscribe('cart.opened', function() {
+      // Only add the content once
+      Snipcart.unsubscribe('cart.opened');
 
-        // Append custom HTML content to cart
-        var html = $("#cart-content-text").html();
-        $(html).insertAfter($("#snipcart-header"));
+      // Append custom HTML content to cart
+      var html = $("#cart-content-text").html();
+      $(html).insertAfter($("#snipcart-header"));
+    });
 
-        // Close cart when logged out
-        Snipcart.subscribe('user.loggedout', function() {
-          Snipcart.api.modal.close();
-        });
+    // Close cart when logged out
+    Snipcart.subscribe('user.loggedout', function() {
+      Snipcart.api.modal.close();
     });
   });
 })(jQuery)
