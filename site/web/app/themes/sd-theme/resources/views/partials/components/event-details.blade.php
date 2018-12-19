d34ad76622cb205ca21ed45ec98ec71aa4fd32d0
briggySmalls <wasso14@hotmail.com>
2018-12-19 01:56:18 +0000

    Add nofollow to venue links

--- site/web/app/themes/sd-theme/resources/views/partials/components/event-details.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/components/event-details.blade.php
@@ -10,7 +10,12 @@
   @if ($post->venue())
     <span>
       @if (isset($full_event_details))
-        <a href="{{ $post->venueUrl() }}" target="_blank" rel="noopener noreferrer">
+        {{-- Some notes on external links
+          noopener: Protect against external links trying to navigate this page
+          noreferrer: Don't 'refer' this site to the linked site
+          nofollow: Stop google following this link as an 'endorsement'
+          --}}
+        <a href="{{ $post->venueUrl() }}" target="_blank" rel="noopener noreferrer nofollow">
       @endif
       {{ $post->venue() }}
       @if (isset($full_event_details))
