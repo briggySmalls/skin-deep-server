874da5e05e68ef19530f053ebc1cf1e2a78a6704
briggySmalls <wasso14@hotmail.com>
2018-12-19 00:43:56 +0000

    Update snipcart script for convenience

--- site/web/app/mu-plugins/skin-deep/resources/assets/scripts/snipcart.js
+++ site/web/app/mu-plugins/skin-deep/resources/assets/scripts/snipcart.js
@@ -1,21 +1,22 @@
 /**
  * Simple script to add custom HTML to the cart
- * @note       This script is loaded with async, after snipcart
  */
 /*global Snipcart*/
-jQuery('#snipcart').load(() => {
-  console.assert(typeof Snipcart !== 'undefined', "Snipcart not found");
-  Snipcart.execute('bind', 'cart.opened', function() {
-      // Immediately unbind this event
-      Snipcart.execute('unbind', 'cart.opened');
+(function($) {
+  $('#snipcart').load(() => {
+    console.assert(typeof Snipcart !== 'undefined', "Snipcart not found");
+    Snipcart.execute('bind', 'cart.opened', function() {
+        // Immediately unbind this event
+        Snipcart.execute('unbind', 'cart.opened');
 
-      // Append custom HTML content to cart
-      var html = jQuery("#cart-content-text").html();
-      jQuery(html).insertAfter(jQuery("#snipcart-header"));
+        // Append custom HTML content to cart
+        var html = $("#cart-content-text").html();
+        $(html).insertAfter($("#snipcart-header"));
 
-      // Close cart when logged out
-      Snipcart.subscribe('user.loggedout', function() {
-        Snipcart.api.modal.close();
-      });
+        // Close cart when logged out
+        Snipcart.subscribe('user.loggedout', function() {
+          Snipcart.api.modal.close();
+        });
+    });
   });
-})
+})(jQuery)
