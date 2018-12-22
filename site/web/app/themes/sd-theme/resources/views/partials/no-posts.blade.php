e8d3967346cd2638f52168dcd572a8079c310d92
briggySmalls <wasso14@hotmail.com>
2018-12-22 14:11:09 +0000

    Fix bug in no-posts template

--- site/web/app/themes/sd-theme/resources/views/partials/no-posts.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/no-posts.blade.php
@@ -1,6 +1,6 @@
 @if (!have_posts())
   <div class="alert alert-warning">
-    @if (\SkinDeep\Events\Plugin::isValidEventPage())
+    @if (\SkinDeep\Events\EventsModule::isValidEventPage())
         {{ __('Sorry, no upcoming events', 'sage') }}
     @else
         {{ __('Sorry, no results were found.', 'sage') }}
