340296dabe934984945e48b7bde09090c96fa340
briggySmalls <wasso14@hotmail.com>
2018-12-24 13:29:12 +0000

    Implement simple popup with custom Mailchimp form

---
+++ site/web/app/themes/sd-theme/resources/views/partials/components/mailing_list.blade.php
@@ -0,0 +1,16 @@
+<div id="mailing-list" class="alert alert-info alert-dismissible fade show" role="alert">
+  <h4 class="alert-heading">Never miss out on the best of Skin Deep</h4>
+  <p>Get it delivered straight to your inbox once a month</p>
+  <form class="form-inline" action="https://skindeepmag.us12.list-manage.com/subscribe/post" method="POST">
+    <input type="hidden" name="u" value="52a202f000bbf7fb27d49f694">
+    <input type="hidden" name="id" value="943ad6701c">
+    <div class="form-group">
+      <label for="mailingEmail" class="sr-only">Email address:</label>
+      <input type="email" id="mailingEmail" placeholder="Enter email" name="MERGE0">
+    </div>
+    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+  </form>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
