c199c342f43273560561b30f91e4dc4c328e9532
briggySmalls <wasso14@hotmail.com>
2018-12-27 01:39:48 +0000

    Add margin

--- site/web/app/themes/sd-theme/resources/views/partials/components/mailing_list.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/components/mailing_list.blade.php
@@ -4,7 +4,7 @@
   <form class="form-inline" action="https://skindeepmag.us12.list-manage.com/subscribe/post" method="POST">
     <input type="hidden" name="u" value="52a202f000bbf7fb27d49f694">
     <input type="hidden" name="id" value="943ad6701c">
-    <div class="form-group">
+    <div class="form-group mr-sm-2">
       <label for="mailingEmail" class="sr-only">Email address:</label>
       <input type="email" class="form-control" id="mailingEmail" placeholder="Enter email" name="MERGE0">
     </div>
