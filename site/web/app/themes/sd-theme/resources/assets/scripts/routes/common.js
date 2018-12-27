3861e04070bf614508e76510156a4d8971cbb1e7
briggySmalls <wasso14@hotmail.com>
2018-12-27 03:10:38 +0000

    Remember user's decision to hide prompt
    Currently saving in browser localStorage, which could be 'forever'
    Alternative is to use a cookie that expires after some time, although
    this is not that nice, as HTTP requests get cluttered with junk.
    Finally, sessionStorage could be used instead, to remember while browser
    is open

--- site/web/app/themes/sd-theme/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/sd-theme/resources/assets/scripts/routes/common.js
@@ -1,10 +1,24 @@
+import storage from 'local-storage-fallback'
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Remove 'shares' count from facebook buttons
     $('.fb-share-button').each(function() {
-      // Remove 'shares' count from facebook buttons
       $(this).attr('data-layout', 'button');
     });
+
+    // Listen for close events on mailing list form
+    $('#mailing-list').on('closed.bs.alert',function() {
+      // Remember that the user has dismissed the alert
+      storage.setItem('mailingListDismissed', true);
+    })
+
+    // Hide/show mailing list based on storage value
+    if (!storage.getItem('mailingListDismissed')) {
+      $('#mailing-list').show();
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
