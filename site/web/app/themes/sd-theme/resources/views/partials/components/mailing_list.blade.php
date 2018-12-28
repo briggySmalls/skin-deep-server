aa131d2156bf1e984502fe674b1576ddb76934f0
briggySmalls <wasso14@hotmail.com>
2018-12-28 01:18:12 +0000

    Improve button alignment on smaller screens
    Integrate submit button with email entry field

--- site/web/app/themes/sd-theme/resources/views/partials/components/mailing_list.blade.php
+++ site/web/app/themes/sd-theme/resources/views/partials/components/mailing_list.blade.php
@@ -2,14 +2,20 @@
   <h4 class="alert-heading">Never miss out on the best of Skin Deep</h4>
   <p>Get it delivered straight to your inbox once a month</p>
   <form class="form-inline" action="https://skindeepmag.us12.list-manage.com/subscribe/post" method="POST">
+    {{-- Hidden elements required by MailChimp --}}
     <input type="hidden" name="u" value="52a202f000bbf7fb27d49f694">
     <input type="hidden" name="id" value="943ad6701c">
-    <div class="form-group mr-sm-2">
+
+    <div class="form-group input-group">
+      {{-- Input: note that name must match MailChimp --}}
       <label for="mailingEmail" class="sr-only">Email address:</label>
       <input type="email" class="form-control" id="mailingEmail" placeholder="Enter email" name="MERGE0">
+      <div class="input-group-append">
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+      </div>
     </div>
-    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
   </form>
+  {{-- Dismiss button for alert --}}
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
