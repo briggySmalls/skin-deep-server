4a4083b1cebdc350d828abff640178a0a2f2d7b1
briggySmalls <wasso14@hotmail.com>
2019-06-25 23:55:32 +0100

    Fix #203: Only display authors if present

--- site/web/app/mu-plugins/skin-deep/resources/views/suggestions-public.blade.php
+++ site/web/app/mu-plugins/skin-deep/resources/views/suggestions-public.blade.php
@@ -4,21 +4,23 @@
 @if (is_single())
   <aside class="suggestions jumbotron">
     <h3>Want to read more?</h3>
-    <p class="lead">
-      More articles written by
-      @foreach ($authors as $author)
-        <a href="{{ get_term_link($author) }}">
-          {{ $author->name }}</a>@if (!$loop->last)<span>,&#32;</span>@endif
-      @endforeach
-    </p>
-    <p class="lead">
-      @if ($categories)
+    @if ($authors)
+      <p class="lead">
+        More articles written by
+        @foreach ($authors as $author)
+          <a href="{{ get_term_link($author) }}">
+            {{ $author->name }}</a>@if (!$loop->last)<span>,&#32;</span>@endif
+        @endforeach
+      </p>
+    @endif
+    @if ($categories)
+      <p class="lead">
         More articles in categories:
         @foreach ($categories as $category)
             <a href="{{ get_category_link($category) }}">
               {{ $category->name }}</a>@if (!$loop->last)<span>,&#32;</span>@endif
         @endforeach
-      @endif
-    </p>
+      </p>
+    @endif
   </aside>
 @endif
