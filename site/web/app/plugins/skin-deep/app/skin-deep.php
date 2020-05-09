a02418f8c90457f6431bddc9d0866a94181b2fb3
briggySmalls <briggySmalls90@gmail.com>
2020-05-09 17:54:07 +0100

    Fix loading skin deep plugin

--- site/web/app/plugins/skin-deep/app/skin-deep.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace SkinDeep;
-
-// Include autoloader
-require __DIR__ . '/../vendor/autoload.php';
---
+++ site/web/app/plugins/skin-deep/skin-deep.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: Skin Deep Plugin
+ * Plugin URI: https://github.com/briggySmalls/skin-deep-server/
+ * Description: Bespoke plugin providing Skin Deep's custom features
+ * Version: 1.0.0
+ * Author: Sam Briggs
+ * Author URI: https://github.com/briggySmalls/
+ * License: MIT License
+ * License URI: http://opensource.org/licenses/MIT
+ */
+
+namespace SkinDeep;
+
+// Include autoloader
+require __DIR__ . '/vendor/autoload.php';
